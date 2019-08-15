ed967a0d8eb8337bdc0ffca284e91a89990181f4
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-08-15 21:58:01 +0900

    feat: beforeEnter in router

--- src/router.js
+++ src/router.js
@@ -2,15 +2,23 @@
 
 import Vue from 'vue';
 import Router from 'vue-router';
+
+import store from './store';
 import Index from './pages/Index.vue';
 
 Vue.use(Router);
+
+const beforeEnter = (to, from, next) => {
+  const isAuth = store.state.isLogin;
+  isAuth ? next() : next({ name: 'login' })
+}
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'index',
+      beforeEnter,
       component: Index,
     },
     {
